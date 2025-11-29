d570f89a635e1260bc8989a2f0f802ba5fc9d6b7
lavindu perera <lavi156perera@gmail.com>
2025-11-29 23:53:25 +0530

    started working on counting probability

---
+++ dice_rolls.py
@@ -0,0 +1,5 @@
+import random
+
+diceNums = [1, 2, 3, 4, 5, 6]
+def roll_dice():
+    return random.choice(diceNums)
---
+++ main.py
@@ -0,0 +1,11 @@
+from dice_rolls import roll_dice
+
+if __name__ == "__main__":
+    count = 0
+    for i in range(10):
+        rolled_number = roll_dice()
+        count += rolled_number
+        print(f"You rolled a {rolled_number}")
+        
+
+    print(f"Total count: {count}")
